b6d1b8b1cd6ef5b3e2296c14fd00efeddfe294af
naylaharyadi <haryadinayla@outlook.Com>
2024-10-02 15:11:17 +0200

    exo4

---
+++ exo4.py
@@ -0,0 +1,10 @@
+nombre = float(input("Ecrivez un nombre : "))
+
+if nombre > 0:
+    print ("Le nombre est positif.")
+
+elif nombre < 0:
+    print ("Le nombre est négatif.")
+
+else:
+    print ("Le nombre est nul.")
